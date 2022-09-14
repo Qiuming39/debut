1c1d6f21104c072aacee55559d3f39b6a870de25
Qiuming <yjh1104086192@gmail.com>
2022-09-14 20:56:53 +0200

    函数和数组

---
+++ 9.14/9.14/函数.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+
+//函数
+//数学:f(x)=2*x+5
+//c:f(x,y) = x+y ---- Add(x,y) = x+y
+
+
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;//因为返回的z是整型，所以Add也得是整型
+//
+//}
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	
+//	scanf("%d%d",&num1,&num2);
+//	//int A = num1 + num2;
+//	//printf("%d\n",A);
+//
+//	//函数的方法
+//	int A = Add(num1, num2);
+//	printf("%d\n", A);
+//	return 0;
+//
+//}
