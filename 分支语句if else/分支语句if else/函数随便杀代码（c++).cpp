ecc828fd3fc70fcae8707ea5ab72f48ad07c4df2
Qiuming39 <yjh1104086192@gmail.com>
2022-09-18 22:01:32 +0200

    未开始，涛哥讲了一些C++的代替东西

---
+++ 分支语句if else/分支语句if else/函数随便杀代码（c++).cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<cstdio>
+#include<iostream>
+using namespace std;
+
+//int main()
+//{
+//	int a = 0, b = 0, c = 0;
+//	cin >> a >> b;
+//	c = a + b;
+//	cout << c << a;
+//	return 0;
+//}
+
+//	//python:
+//  //>>a >>b
+//	//c enter
--- 指针和结构体/指针和结构体/结构体.c
+++ 指针和结构体/指针和结构体/结构体.c
@@ -20,8 +20,8 @@ struct Book
 };
 int main()
 {
-	struct stu s = { "张三", 20, 85.5 };//结构体的创建和初始化
-	printf("1:%s %d %lf\n", s.name, s.age, s.score);//结构体变量，成员变量
+	struct stu s = { "张三", 20, 85.5 };//结构体的创建和初始化 s为结构体变量
+	printf("1:%s %d %lf\n", s.name, s.age, s.score);//结构体变量.成员变量
 	struct stu* ps = &s;
 	printf("2:%s %d %lf\n", (*ps).name, (*ps).age, (*ps).score);
 	printf("3:%s %d %lf\n", ps->name, ps->age, ps->score);//相当于包含了*和.//左边一定是结构体指针->成员变量名
